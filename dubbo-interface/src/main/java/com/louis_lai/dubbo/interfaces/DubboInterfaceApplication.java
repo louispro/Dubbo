535c963b55e7085793d43a7c2c59ead4828a7530
laixiaoyi <laixiaoyi@xiongmaitech.com>
2022-11-23 20:39:05 +0800

    project init

---
+++ dubbo-interface/src/main/java/com/louis_lai/dubbo/interfaces/DubboInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.louis_lai.dubbo.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboInterfaceApplication.class, args);
+    }
+
+}
